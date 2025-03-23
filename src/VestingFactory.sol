faa73175dc8a41c542f035bbbd1c4addd1d8200c
danilych <danilych45@proton.me>
2025-03-23 01:37:48 +0100

    feat: emit improvements

--- src/VestingFactory.sol
+++ src/VestingFactory.sol
@@ -3,13 +3,24 @@ pragma solidity 0.8.22;
 
 import { Ownable } from "@openzeppelin-contracts/access/Ownable.sol";
 import { IERC20 } from "@openzeppelin-contracts/token/ERC20/IERC20.sol";
+import { IERC20Metadata } from "@openzeppelin-contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 import { Vesting } from "./Vesting.sol";
 import { Errors } from "./libraries/Errors.sol";
 
 contract VestingFactory is Ownable {
     event VestingContractCreated(address indexed owner, address indexed vestingContract, address indexed token);
-    event VestingScheduleCreated(address indexed vestingContract, address indexed beneficiary, uint256 totalAmount);
+    event VestingScheduleCreated(
+        address indexed vestingContract,
+        address indexed beneficiary,
+        uint256 totalAmount,
+        uint256 start,
+        uint256 periodDuration,
+        uint256 totalPeriods,
+        string tokenName,
+        string tokenSymbol,
+        address token
+    );
 
     constructor() Ownable(msg.sender) { }
 
@@ -69,7 +80,17 @@ contract VestingFactory is Ownable {
             _beneficiary, _start, _periodDuration, _totalPeriods, _totalAmount, _unlockAmounts
         );
 
-        emit VestingScheduleCreated(vestingContractAddress, _beneficiary, _totalAmount);
+        emit VestingScheduleCreated(
+            vestingContractAddress,
+            _beneficiary,
+            _totalAmount,
+            _start,
+            _periodDuration,
+            _totalPeriods,
+            IERC20Metadata(_token).name(),
+            IERC20Metadata(_token).symbol(),
+            _token
+        );
 
         return vestingContractAddress;
     }
